32e2300ee0fbd866bd6766e2395744d18c77832f
lillianxhub <93813368+lillianxhub@users.noreply.github.com>
2026-09-10 13:24:50 +0700

    Initial Commit

---
+++ src/main/java/th/ac/kku/freelance_hub/FreelanceHubApplication.java
@@ -0,0 +1,13 @@
+package th.ac.kku.freelance_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelanceHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelanceHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/th/ac/kku/freelance_hub/FreelanceHubApplicationTests.java
@@ -0,0 +1,13 @@
+package th.ac.kku.freelance_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelanceHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
