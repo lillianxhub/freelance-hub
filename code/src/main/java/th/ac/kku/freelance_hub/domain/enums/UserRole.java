57db4cea7ee31a0b82c833476ed6d8a3e0c4be85
lillianxhub <93813368+lillianxhub@users.noreply.github.com>
2026-09-18 23:46:46 +0700

    Add User and UserProfile entities with role and status enums

---
+++ code/src/main/java/th/ac/kku/freelance_hub/domain/enums/UserRole.java
@@ -0,0 +1,10 @@
+package th.ac.kku.freelance_hub.domain.enums;
+
+/**
+ * User role enum for authorization
+ * MVP supports only USER role
+ */
+public enum UserRole {
+    USER,
+    ADMIN // Reserved for future use
+}
