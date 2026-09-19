16600fe5e15636f5925aa29a1880041be3791ed8
lillianxhub <93813368+lillianxhub@users.noreply.github.com>
2026-09-19 21:14:56 +0700

    feat: update configuration for Supabase integration and environment variables

--- code/Backend/src/main/java/th/ac/kku/freelance_hub/FreelanceHubApplication.java
+++ code/Backend/src/main/java/th/ac/kku/freelance_hub/FreelanceHubApplication.java
@@ -1,5 +1,6 @@
 package th.ac.kku.freelance_hub;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FreelanceHubApplication {
 
 	public static void main(String[] args) {
+		// Load env
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(FreelanceHubApplication.class, args);
 	}
 
